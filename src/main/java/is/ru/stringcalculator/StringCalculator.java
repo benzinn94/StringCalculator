4b15dba440bd3e88bc72b1c1da0b98251547f9bd
Benedikt Þorri Þórarinsson <benedikt@Benedikts-MacBook-Pro.local>
2017-10-12 13:37:27 +0000

    Step ii. An empty string will return 0 implemented with a test, successfully.

--- src/main/java/is/ru/stringcalculator/StringCalculator.java
+++ src/main/java/is/ru/stringcalculator/StringCalculator.java
@@ -3,10 +3,17 @@ package is.ru.stringcalculator;
 public class StringCalculator{
 	
 	public static int add(String text){
+		if(text.equals("")){
+			return 0;
+		}
+		if(text.contains(",")){
+			
+		}
 		return 0;
 	}
 
+
 	public static void main(String[] args){
-		
+
 	}
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -4,4 +4,9 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 public class CalculatorTest{
 
+	@Test
+	public void testEmptyString(){
+		assertEquals(0, StringCalculator.add(""));
+	}
+
 }
